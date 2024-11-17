b12e262f563246fb4bb457c3fa30a9a3149f4901
Bebs Fiorillo <estevaovrb2014@gmail.com>
2024-11-17 19:51:54 -0300

    deleteNode pronto

--- ListaContigua.cpp
+++ ListaContigua.cpp
@@ -18,8 +18,13 @@ ListaContigua::~ListaContigua()
 int ListaContigua::get(int p)
 {
     if (p<0 || p>=n)
-        return NULL;
+        exit(1);
     return vet[p];
+}
+
+int ListaContigua::getSize()
+{
+    return n;
 }
 
 void ListaContigua::set(int p, int val)
@@ -47,9 +52,26 @@ void ListaContigua::deleteEnd()
 void ListaContigua::newNode(int p, int val)
 {
     if(p<0 || p>=n || n==max)
+    {
         cout << "Invalido" << endl; //nao fiz separado por preguica, tinha esquecido de verificar se estava cheio
         return;
+    }
     for (int i=p;i<n;i++)
         vet[i+1]=vet[i];
     vet[p]=val;
+    n++;
+}
+
+void ListaContigua::deleteNode (int p)
+{
+    if(p<0 || p>=n || n==max)
+    {
+        cout << "Invalido" << endl; //nao fiz separado por preguica, copiei do anterior
+        return;
+    }
+    //for (int i=n-1;i>=p;i--) nn funciona pois aqui quando ele for ir passando pra tras, o valor do "i" sera sempre o do ultimo
+    //    vet[i-1]=vet[i];      por exemplo, o ultimo se copia pro penultimo. dai o penultimno se copia pro antipenultimo, mas ja seria o valor modificado, ou seja, todos seriam o ultimo
+    for (int i=p;i<n;i++)
+        vet[i]=vet[i+1];
+    n--;
 }
--- ListaContigua.h
+++ ListaContigua.h
@@ -10,11 +10,12 @@ class ListaContigua
         ListaContigua(int max);
         ~ListaContigua();
         int get(int p);
+        int getSize();
         void set(int p, int val);
         void newEnd(int val);
         void deleteEnd();
         void newNode(int p, int val);
-        void deleteNode(int p, int val);
+        void deleteNode(int p);
 
     private:
         int n;
