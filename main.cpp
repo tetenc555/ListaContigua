53447866e4c5ed3b3446f292ef99c62b99e9fe16
Bebs Fiorillo <estevaovrb2014@gmail.com>
2024-11-17 19:40:38 -0300

    prepara main pra teste deleteNode

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main()
 {
-    
+    ListaContigua geraldo(7);
+    geraldo.newEnd(1);
+    geraldo.newEnd(1);
+    geraldo.newEnd(2);
+    geraldo.newEnd(1);
+    geraldo.deleteNode(1);
+    for (int i=0;i<geraldo.getSize();i++)
+        cout << geraldo.get(i);;
     return 0;
 }
